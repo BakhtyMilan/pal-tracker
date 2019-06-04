c8db807a107e7321d5673152ea349e4aecc2e61e
BakhtyMilan <mr.bakhtiyarhuseynov@gmail.com>
2019-06-04 16:21:09 -0400

    For Travis run

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
     @GetMapping("/")
     public String sayHello() {
-        return "hello Bakhty";
+        return "Hello";
     }
 
 
